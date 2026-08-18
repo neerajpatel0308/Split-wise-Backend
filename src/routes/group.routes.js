a02139d2ac2d6b456a81da5dc7e439b33f214e59
neerajcv <neeraj@cvinfotech.com>
2026-08-18 17:25:38 +0530

    Typo mistake

--- src/routes/group.routes.js
+++ src/routes/group.routes.js
@@ -11,7 +11,7 @@ import { protect } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.post("/", protect, createGroup);
 router.get("/", protect, getGroups);
-router.get("/:grouId", protect, getGroupById);
+router.get("/:groupId", protect, getGroupById);
 
 router.post("/:groupId/members", protect, addMember);
 router.get("/:groupId/dashboard", protect, getGroupDashboard);
