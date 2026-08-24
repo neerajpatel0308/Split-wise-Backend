58c8d1857bc49b7bef15b8a307a11e5e14c4d4cf
neerajcv <neeraj@cvinfotech.com>
2026-08-24 17:11:04 +0530

    otp update

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -17,8 +17,8 @@ import {
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/verify-otp", verifyOTP);
-router.post("/verify-email", verifyEmail);
+router.post("/verify-email", verifyOTP);
+router.post("/verify-otp", verifyEmail);
 router.post("/resend-otp", resendOTP);
 router.post("/login", login);
 router.post("/logout", protect, logout);
