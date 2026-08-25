ced00980018d8c42a28782be050af1d8c5f5fb68
Ankit-Dokarka <ankit@cvinfotech.com>
2026-08-25 15:30:40 +0530

    final test

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -180,12 +180,7 @@ export const login = async (req, res) => {
 
     const token = generateToken(user._id);
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+   res.cookie("token", token, cookieOptions);
 
     res.status(200).json({
       success: true,
